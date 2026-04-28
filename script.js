08286bb2398a36c6d992b8f1d98d9307b20e47f7
kritika <34717469+kritikabhat@users.noreply.github.com>
2026-04-28 12:51:16 -0400

    feat: currentDisplay now shows values

--- script.js
+++ script.js
@@ -24,23 +24,73 @@
 const operatorsContainer = document.querySelector('.operatorsContainer')
 const otherContainer = document.querySelector('#otherContainer')
 
+const operations = ['+', '-', '/', 'x']
+let operand1 = ''
+let operand2 = ''
+let operator = ''
+let result = ''
+
 // The numbers, operand are shown on the Current Display now
 operatorsContainer.addEventListener('click', (e) => {
-    otherContainer.querySelector('#currentDisplay').textContent += `${e.target.textContent}`
+    // equals should work differently
+    if (e.target.matches('#equalsKey')) {
+        console.log("Equals key")
+        // likely the calc method will be called here
+        if (operand1 && operand2 && operator) {
+            calculate(operand1, operand2, operator)
+        }
+
+        operand1 = result
+        operand2 = ''
+        result = ''
+        operator = ''
+    } else if (e.target.matches('.operation') && !operator) { // +, -, x, /
+        operator = e.target.textContent
+        otherContainer.querySelector('#currentDisplay').textContent += `${e.target.textContent}`
+    } else { // [0-9], . 
+        if (!operator) {
+            operand1 += e.target.textContent
+            otherContainer.querySelector('#currentDisplay').textContent += `${e.target.textContent}`
+        } else {
+            operand2 += e.target.textContent
+            otherContainer.querySelector('#currentDisplay').textContent += `${e.target.textContent}`
+        }
+    }
+    console.log("operand1: " + operand1)
+    console.log("operator: " + operator)
+    console.log("operand2: " + operand2)
 })
+
+
+
+/**
+ * First operator- always allowed
+ * Two operators together- not allowed: write a check for this
+ * if there are two numbers and an operator between them,
+ *      instead of disabling operators, when another operator is pressed
+ *      the current operation should be completed in the finalDisplay
+ *      and that final value should be shown in the currentValue
+ * 
+ * If you don't do this, and make it work like your phone calc then the box
+ *      size will cause overflow.
+ * 
+ * can use contains to check for the presence of operators
+ *
+ */
 
 otherContainer.addEventListener('click', (e) => {
     if (e.target.textContent === "CLEAR") {
         otherContainer.querySelector('#currentDisplay').textContent = ''
         otherContainer.querySelector('#finalDisplay').textContent = '0'
+        operand1 = ''
+        operand2 = ''
+        result = ''
+        operator = ''
     }
-        
-
+    if (e.target.textContent === "Neg") {
+        otherContainer.querySelector('#currentDisplay').textContent += `-`
+    }
 })
-
-let operand1
-let operand2
-let operator
 
 // You don't need named functions for this calc, can put func exp in the switch case
 function add(a, b) {
@@ -59,7 +109,7 @@ function divide(a, b) {
     return a / b
 }
 
-function operate (operand1, operand2, operator) {
+function calculate (operand1, operand2, operator) {
     switch(operator) {
         case '+' : {
             add(operand1, operand2);
