9878de0352cd3fe217325e07204f3e867cdc1d3d
kritika <34717469+kritikabhat@users.noreply.github.com>
2026-04-28 14:53:10 -0400

    feat: functionality added to Negative button, including with complex arithematic

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
 /**
- * Goals: Currently, the currentDisplay only allows a * b operation.
- * Then all bts are disabled. Change this so complex things can be done?
- * Ignore the bracket need. Just go by DMAS
- * 
- * The FinalDisplay also doesn't allow you to use it's value as a starting point
- * Allow that
- * 
- * Update the ID, class names on the html tags
- * There are many redundant names there
- * subtract button currently has a id which seems to be unused. look into that
- */
-
-/**
- * container has otherContainer and operatorsContainer
- * otherContainer has resultBox (which has currentDisplay and FinalDisplay)
- * 
- * currentDisplay shows like 7 x 8
- * finalDisplay has 56
- * 
- * clearSignContainer has a clear btn and a sign btn
+ * Update the ID, class names on the html tags. There are many redundant names there
  */
 
 const operatorsContainer = document.querySelector('.operatorsContainer')
@@ -29,6 +10,7 @@ let operand1 = ''
 let operand2 = ''
 let operator = ''
 let result = ''
+let currentSign = '+'
 
 operatorsContainer.addEventListener('click', (e) => {
     if (e.target.matches('#equalsKey')) {
@@ -36,13 +18,14 @@ operatorsContainer.addEventListener('click', (e) => {
             result = calculate(operand1, operand2, operator)
             otherContainer.querySelector('#finalDisplay').textContent = result
 
-            // Once the calculation is complete, you want to reset the values
-            // Unless the clear button is pressed, you continue so
+            // This allows additional arithematic using the first result value
             operand1 = result
             operand2 = ''
             operator = ''
+            currentSign = (+result > 0) ? '+' : '-'
+            console.log("result now: " + result)
+            console.log("Current sign: " + currentSign)
         }
-
     } else if (e.target.matches('.operation') && !operator) { // +, -, x, /
         if (!result) {
             operator = e.target.textContent
@@ -63,22 +46,6 @@ operatorsContainer.addEventListener('click', (e) => {
     }
 })
 
-
-
-/**
- * Two operators together- not allowed: write a check for this
- * if there are two numbers and an operator between them,
- *      instead of disabling operators, when another operator is pressed
- *      the current operation should be completed in the finalDisplay
- *      and that final value should be shown in the currentValue
- * 
- * If you don't do this, and make it work like your phone calc then the box
- *      size will cause overflow.
- * 
- * can use contains to check for the presence of operators
- *
- */
-
 otherContainer.addEventListener('click', (e) => {
     if (e.target.textContent === "CLEAR") {
         otherContainer.querySelector('#currentDisplay').textContent = ''
@@ -88,8 +55,42 @@ otherContainer.addEventListener('click', (e) => {
         result = ''
         operator = ''
     }
+
     if (e.target.textContent === "Neg") {
-        otherContainer.querySelector('#currentDisplay').textContent += `-`
+        if (!result) { // first arithematic operation
+            if (currentSign === "+") {
+                if (!operand1) {
+                    operand1 = "-"   
+                } else if (operand1 && !operator) {
+                    operand1 = "-" + operand1
+                }
+                otherContainer.querySelector('#currentDisplay').textContent = operand1
+                currentSign = "-"
+            }
+        } else { // "result" is used to allow additional arithematic
+            console.log("result so")
+            if (currentSign === "+") {
+                if (!operator) {
+                    result = "-" + result
+                    operand1 = result
+                    otherContainer.querySelector('#currentDisplay').textContent = result
+                    otherContainer.querySelector('#finalDisplay').textContent = result
+                }
+            }
+            if (currentSign === "-") { // changes result sign to positive during additional arithematic
+                if(!operator) {
+                    result = result.toString().slice(1)
+                    operand1 = result
+                    otherContainer.querySelector('#currentDisplay').textContent = result
+                    otherContainer.querySelector('#finalDisplay').textContent = result
+                }
+
+            }
+        }  
+        if (operator && !operand2) { // this allows operand2 to be negative
+            operand2 = "-"
+            otherContainer.querySelector('#currentDisplay').textContent += "-"
+        } 
     }
 })
 
